56dc2115de2dc986069cccb0e0402b6f139cd0d6
ExplorerManasa <tripuramallumanasa@gmail.com>
2022-11-30 12:00:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+/*import ClassComponent from './classcomponent'
+import ClassComponent2 from './classcomponent2'
+function App(){
+  return(
+    <section>
+      <p>good morning</p>
+      <h1>welcome</h1>
+      <ClassComponent/>
+       <ClassComponent2/>
+      <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTyE_6kXJfEibi2aNkg709Re-tiYyT5ywH-z0bojixB-V73Zn48"/>
+    </section>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    )
+  }
+
+export default App*/
+import PropsExample from './PropsExample'
+function App(){
+  return(
+    <div>
+    <h1>This is about cls</h1>
+    <PropsExample name="dhanu" age="20"/>
     </div>
-  );
+    )
 }
-
-export default App;
+export default App
---
+++ src/classcomponent2.js
@@ -0,0 +1,13 @@
+import React from "react"
+class ClassComponent2 extends React.Component{
+	render(){
+		return(
+			<div>
+			From class component dhanu
+
+
+			</div>
+			)
+	}
+}
+export default ClassComponent2
